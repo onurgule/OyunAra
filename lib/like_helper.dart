c13b3082bf00a4a9394fa19a512b67dfe00207fb
omerates760 <33201732+omerates760@users.noreply.github.com>
2020-07-28 14:28:10 +0300

    sharedpreference helper oluşturuldu

---
+++ lib/like_helper.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LikedHelper {
+  Future<String> getJson(String _key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String _json = prefs.getString('$_key') ?? "start";
+    return _json;
+  }
+
+  setJson(String _key, String _json) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('$_key', _json);
+  }
+
+  delJson(String _key) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('$_key');
+  }
+
+  getAllData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final keys = prefs.getKeys();
+
+    final prefsMap = Map<String, dynamic>();
+    for (String key in keys) {
+      prefsMap[key] = prefs.get(key);
+    }
+
+    //print("sonuc" + prefsMap);
+  }
+}
