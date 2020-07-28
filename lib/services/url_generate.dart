c4f97e520fdc4e283d2d4d2a8bede222fb208102
omerates760 <33201732+omerates760@users.noreply.github.com>
2020-07-28 14:38:33 +0300

    Service classı eklendi

---
+++ lib/services/url_generate.dart
@@ -0,0 +1,41 @@
+import 'package:OyunAra/model/popular_filter_list.dart';
+import 'package:flutter/material.dart';
+
+class UrlGenerate {
+  String getGames(
+      List<PopularFilterListData> online,
+      List<PopularFilterListData> type,
+      List<PopularFilterListData> platform,
+      RangeValues players) {
+    //https://oyunara.tk/api/getGames.php?platform=1&online=1&player=5&type=5
+    var url = "https://oyunara.tk/api/getGames.php?";
+    url += "platform=";
+    platform.forEach((element) {
+      url += (element.isSelected) ? element.value.toString() + "," : '';
+    });
+    url = (url.endsWith(',')) ? url.substring(0, url.lastIndexOf(',')) : url;
+
+    url += "&online=";
+    online.forEach((element) {
+      url += (element.isSelected) ? '1' + "," : '0';
+    });
+    url = (url.endsWith(',')) ? url.substring(0, url.lastIndexOf(',')) : url;
+
+    url += "&playermin=" +
+        players.start.toInt().toString() +
+        "&playermax=" +
+        players.end.toInt().toString();
+    url = (url.endsWith(',')) ? url.substring(0, url.lastIndexOf(',')) : url;
+
+    url += "&type=";
+    type.forEach((element) {
+      url += (element.isSelected) ? element.value.toString() + "," : '';
+    });
+    url = (url.endsWith(',')) ? url.substring(0, url.lastIndexOf(',')) : url;
+    return url;
+  }
+
+  String getAllCategories() {
+    return "https://oyunara.tk/api/getOptions.php?opt=t";
+  }
+}
